7ea406a66807dfd0ebe91834cd886f03c6d11c53
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-11-02 12:37:39 -0600

    Angular Reference

--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,15 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
-  
+angular.module('devmtnTravel').service('mainSrvc', function($http) {
+  this.getTravelInfo = function() {
+    return $http.get('https://practiceapi.devmountain.com/api/travel/travel-info')
+        .then(response => {
+            return response
+        })
+  }
+
+  this.getPackageInfo = function(){
+    return $http.get('https://practiceapi.devmountain.com/api/travel/package-info')
+        .then(response => {
+            return response
+        })
+  }
 });
